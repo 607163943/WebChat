4173b640d0c7195c519957f9af65136fbd13ab66
607163943 <q45962219@126.com>
2026-09-17 14:09:22 +0800

    搭建项目初始开发环境

---
+++ webchat-server/src/main/java/com/webchat/WebchatServerApplication.java
@@ -0,0 +1,13 @@
+package com.webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebchatServerApplication.class, args);
+    }
+
+}
